8bddefbc24e1d518e61438d8f1ca680c1fe1c51d
markus <mhofmann@b-factor.de>
2020-04-16 01:17:05 +0200

    update the palettes for variants

--- Configuration/TCA/Overrides/sys_file_reference.php
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -3,6 +3,10 @@
 declare(strict_types=1);
 
 call_user_func(function () {
+    $GLOBALS['TCA']['sys_file_reference']['palettes']['variantsPalette'] = [
+        'label' => 'LLL:EXT:responsive_picture/Resources/Private/Language/locallang_tca.xlf:sys_file_reference.variantsPalette',
+        'showitem' => 'media_width,--linebreak--,crop'
+    ];
     $newColumns = [
         'picture_variants' => [
             'label' => 'LLL:EXT:responsive_picture/Resources/Private/Language/locallang_tca.xlf:sys_file_reference.picture_variants',
@@ -16,32 +20,32 @@ call_user_func(function () {
                     'types' => [
                         '0' => [
                             'showitem' => '
-                                --palette--;;imageoverlayPalette,
+                                --palette--;;variantsPalette,
                                 --palette--;;filePalette'
                         ],
                         \TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => [
                             'showitem' => '
-                                --palette--;;imageoverlayPalette,
+                                --palette--;;variantsPalette,
                                 --palette--;;filePalette'
                         ],
                         \TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
                             'showitem' => '
-                                --palette--;;imageoverlayPalette,
+                                --palette--;;variantsPalette,
                                 --palette--;;filePalette'
                         ],
                         \TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => [
                             'showitem' => '
-                                --palette--;;audioOverlayPalette,
+                                --palette--;;variantsPalette,
                                 --palette--;;filePalette'
                         ],
                         \TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => [
                             'showitem' => '
-                                --palette--;;videoOverlayPalette,
+                                --palette--;;variantsPalette,
                                 --palette--;;filePalette'
                         ],
                         \TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
                             'showitem' => '
-                                --palette--;;imageoverlayPalette,
+                                --palette--;;variantsPalette,
                                 --palette--;;filePalette'
                         ]
                     ],
@@ -58,17 +62,15 @@ call_user_func(function () {
                     ['LLL:EXT:responsive_picture/Resources/Private/Language/locallang_tca.xlf:sys_file_reference.media_width.tablet', '(min-width: 768px)'],
                     ['LLL:EXT:responsive_picture/Resources/Private/Language/locallang_tca.xlf:sys_file_reference.media_width.mobile', '(min-width: 300px)'],
                 ],
-            ],
-            'displayCond' => 'FIELD:tablenames:=:sys_file_reference'
+            ]
         ],
     ];
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_reference', $newColumns);
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('sys_file_reference', 'imageoverlayPalette','--linebreak--,picture_variants', 'after:crop');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('sys_file_reference', 'imageoverlayPalette','media_width', 'after:title');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('sys_file_reference', 'variantsPalette','media_width', 'after:title');
 
     $GLOBALS['TCA']['sys_file_reference']['columns']['link']['displayCond'] = 'FIELD:tablenames:!=:sys_file_reference';
     $GLOBALS['TCA']['sys_file_reference']['columns']['title']['displayCond'] = 'FIELD:tablenames:!=:sys_file_reference';
     $GLOBALS['TCA']['sys_file_reference']['columns']['alternative']['displayCond'] = 'FIELD:tablenames:!=:sys_file_reference';
     $GLOBALS['TCA']['sys_file_reference']['columns']['description']['displayCond'] = 'FIELD:tablenames:!=:sys_file_reference';
-    $GLOBALS['TCA']['sys_file_reference']['columns']['picture_variants']['displayCond'] = 'FIELD:tablenames:!=:sys_file_reference';
 });
