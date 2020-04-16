bca66040e0d72d2d8bef94a74349b04ce8c125b3
Frank Berger <fberger@sudhaus7.de>
2020-04-16 02:05:36 +0200

    working baseline

---
+++ Classes/Overrides/FileReference.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace SUDHAUS7\Picture\Overrides;
+
+use PDO;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Frontend\Resource\FileCollector;
+
+
+class FileReference extends \TYPO3\CMS\Core\Resource\FileReference
+{
+    
+    /**
+     * @var null|array
+     */
+    protected $variants = null;
+    
+    public function __construct(array $fileReferenceData, $factory = null)
+    {
+        parent::__construct($fileReferenceData, $factory);
+        $this->getVariants();
+    }
+    
+    /**
+     * @return array
+     */
+    public function getVariants() : array
+    {
+        
+        if ($this->variants === null) {
+            $this->variants = [];
+    
+            $fileCollector = GeneralUtility::makeInstance(FileCollector::class);
+            $properties = $this->getProperties();
+            $om = GeneralUtility::makeInstance(ObjectManager::class);
+            $db = $om->get(ConnectionPool::class)->getQueryBuilderForTable('sys_file_reference');
+    
+            $result = $db->select('*')
+                ->from('sys_file_reference')
+                ->where(
+                    $db->expr()->andX(...[
+                        $db->expr()->eq('uid_foreign', $properties['uid']),
+                        $db->expr()->eq('tablenames', $db->quote('sys_file_reference'))
+                    ])
+                )
+                ->orderBy('sorting_foreign')
+                ->execute();
+            $uids = [];
+            while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                $uids[]=$row['uid'];
+            }
+            if (!empty($uids)) {
+                $fileCollector->addFileReferences($uids);
+                $this->variants = $fileCollector->getFiles();
+            }
+            
+            
+            
+        }
+    
+      
+      return $this->variants;
+    }
+    
+}
---
+++ ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+
+
+use SUDHAUS7\Picture\Overrides\FileReference;
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Resource\FileReference::class] = [
+    'className' => FileReference::class
+];
