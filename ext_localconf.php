b0091cf7706330be5404eeb43f9b7f23b788b70d
Frank Berger <fberger@sudhaus7.de>
2020-04-16 02:10:47 +0200

    :bug: no use of use in ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,6 @@
 <?php
 
 
-use SUDHAUS7\Picture\Overrides\FileReference;
-
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Core\Resource\FileReference::class] = [
-    'className' => FileReference::class
+    'className' => \SUDHAUS7\Picture\Overrides\FileReference::class
 ];
