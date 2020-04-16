fd5fbb2ac43d694620642807b5e0cf177ad8d098
Frank Berger <fberger@sudhaus7.de>
2020-04-16 02:08:42 +0200

    CS Fixes

--- Classes/Overrides/FileReference.php
+++ Classes/Overrides/FileReference.php
@@ -9,35 +9,33 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Frontend\Resource\FileCollector;
 
-
 class FileReference extends \TYPO3\CMS\Core\Resource\FileReference
 {
-    
+
     /**
      * @var null|array
      */
     protected $variants = null;
-    
+
     public function __construct(array $fileReferenceData, $factory = null)
     {
         parent::__construct($fileReferenceData, $factory);
         $this->getVariants();
     }
-    
+
     /**
      * @return array
      */
-    public function getVariants() : array
+    public function getVariants(): array
     {
-        
         if ($this->variants === null) {
             $this->variants = [];
-    
+
             $fileCollector = GeneralUtility::makeInstance(FileCollector::class);
             $properties = $this->getProperties();
             $om = GeneralUtility::makeInstance(ObjectManager::class);
             $db = $om->get(ConnectionPool::class)->getQueryBuilderForTable('sys_file_reference');
-    
+
             $result = $db->select('*')
                 ->from('sys_file_reference')
                 ->where(
@@ -49,20 +47,15 @@ class FileReference extends \TYPO3\CMS\Core\Resource\FileReference
                 ->orderBy('sorting_foreign')
                 ->execute();
             $uids = [];
-            while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                 $uids[]=$row['uid'];
             }
             if (!empty($uids)) {
                 $fileCollector->addFileReferences($uids);
                 $this->variants = $fileCollector->getFiles();
             }
-            
-            
-            
         }
-    
-      
-      return $this->variants;
+
+        return $this->variants;
     }
-    
 }
