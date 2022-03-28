41ff5595085687b980e0ec55c035b165708bff3c
daniel <dsimon@b-factor.de>
2022-03-28 12:52:53 +0200

    Changed required TYPO3 version

---
+++ Tests/packages/test_site/Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    \SUDHAUS7\TestSite\Domain\Model\Comment::class => [
+        'properties' => [
+            'tstamp' => [
+                'fieldName' => 'tstamp'
+            ],
+        ],
+    ],
+];
--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Sudhaus7, a B-Factor GmbH label https://sudhaus7.de/',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99'
+            'typo3' => '10.4.99-11.5.99'
         ],
         'conflicts' => [
         ],
