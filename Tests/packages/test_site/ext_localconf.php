d1a7a32360e4f26f0218a021cccd250a4d3e9823
Markus Hofmann <m.hofmann@web-vision.de>
2023-02-21 09:39:12 +0100

    [TASK] Increase TYPO3 version to v11 after local testing

--- Tests/packages/test_site/ext_localconf.php
+++ Tests/packages/test_site/ext_localconf.php
@@ -43,19 +43,3 @@ ExtensionUtility::configurePlugin(
     ExtensionUtility::PLUGIN_TYPE_PLUGIN
 );
 
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['guard7'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['guard7'] = [];
-}
-
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['guard7'][] = [
-    'className'=> Blog::class,
-    'tableName'=>'tx_workshopblog_domain_model_blog',
-    'fields'=>'teaser,bodytext'
-];
-/* done with trait and pagets
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['guard7'][] = [
-    'className'=>\SUDHAUS7\TestSite\Domain\Model\Comment::class,
-    'tableName'=>'tx_workshopblog_domain_model_comment',
-    'fields'=>'commentor,comment'
-];
-*/
