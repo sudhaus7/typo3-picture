b2fd606a4051f231bffd7c9e0819c6b49adc3a57
mhofmann <m.hofmann@web-vision.de>
2023-07-27 12:54:16 +0200

    [TASK] Add remove items option

--- Classes/TCA/MediaWidthItemProcFunc.php
+++ Classes/TCA/MediaWidthItemProcFunc.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SUDHAUS7\ResponsivePicture\TCA;
 
 use TYPO3\CMS\Core\Utility\ArrayUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class MediaWidthItemProcFunc
 {
@@ -15,7 +16,14 @@ class MediaWidthItemProcFunc
     {
         $possibleVariants = $GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['cropVariants'] ??= [];
         $itemsArray = [];
+        $removeItems = [];
+        if (isset($configuration['removeItems'])) {
+            $removeItems = GeneralUtility::trimExplode(',', $configuration['removeItems']);
+        }
         foreach ($possibleVariants as $key => $variant) {
+            if (in_array($key, $removeItems)) {
+                continue;
+            }
             $itemsArray[] = [
                 $variant['title'],
                 $key,
